5d397eb8da8319d2cd1aa46d2fe8e6ee2ef0a85a
crispeeweevile <crispeeweevile@gmail.com>
2023-07-29 01:39:40 -0500

    implemented first test

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+#include "linkedsys.h"
 
 int main(int arc, char *arv[]) {
-    
+    {
+        LList *tList = create_llist(create_node(create_data(1, 2, 3)));
+        print_node(tList->current, true);
+    }
 
     return 0;
 }
