7e2981e3d661364320ea32b162ff8f291f46ff86
crispeeweevile <crispeeweevile@gmail.com>
2023-07-29 14:25:44 -0500

    lined up index comments

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ int main(int arc, char *arv[]) {
     {
         LList *tList = create_llist(create_node(create_data(1, 2, 3))); // 0
         print_cur_node(tList, true);
-        insert_at_tail(tList, create_node(create_data(4, 5, 6))); // 1
+        insert_at_tail(tList, create_node(create_data(4, 5, 6)));       // 1
         goto_next(tList);
         printf("went forth\n");
         print_cur_node(tList, true);
@@ -13,17 +13,17 @@ int main(int arc, char *arv[]) {
         printf("went back\n");
         print_cur_node(tList, true);
         printf("insert a 'ton'\n");
-        insert_at_tail(tList, create_node(create_data(4, 5, 6)));    // 2
-        insert_at_tail(tList, create_node(create_data(7, 8, 9)));    // 3
-        insert_at_tail(tList, create_node(create_data(10, 11, 12))); // 4
-        insert_at_tail(tList, create_node(create_data(13, 14, 15))); // 5
-        insert_at_tail(tList, create_node(create_data(16, 17, 18))); // 6
-        insert_at_tail(tList, create_node(create_data(19, 20, 21))); // 7
-        insert_at_tail(tList, create_node(create_data(22, 23, 24))); // 8
-        insert_at_tail(tList, create_node(create_data(25, 26, 27))); // 9
-        insert_at_tail(tList, create_node(create_data(28, 29, 30))); // 10
-        insert_at_tail(tList, create_node(create_data(31, 32, 33))); // 11
-        insert_at_tail(tList, create_node(create_data(34, 35, 36))); // 12
+        insert_at_tail(tList, create_node(create_data(4, 5, 6)));       // 2
+        insert_at_tail(tList, create_node(create_data(7, 8, 9)));       // 3
+        insert_at_tail(tList, create_node(create_data(10, 11, 12)));    // 4
+        insert_at_tail(tList, create_node(create_data(13, 14, 15)));    // 5
+        insert_at_tail(tList, create_node(create_data(16, 17, 18)));    // 6
+        insert_at_tail(tList, create_node(create_data(19, 20, 21)));    // 7
+        insert_at_tail(tList, create_node(create_data(22, 23, 24)));    // 8
+        insert_at_tail(tList, create_node(create_data(25, 26, 27)));    // 9
+        insert_at_tail(tList, create_node(create_data(28, 29, 30)));    // 10
+        insert_at_tail(tList, create_node(create_data(31, 32, 33)));    // 11
+        insert_at_tail(tList, create_node(create_data(34, 35, 36)));    // 12
         FError err = goto_index(tList, 12);
         if (err != FSUCCESS) {
             printf("ooooof\n");
