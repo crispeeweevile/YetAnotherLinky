e38d736187e086f39a577c3c53059ceb933550e1
crispeeweevile <crispeeweevile@gmail.com>
2023-07-29 02:05:07 -0500

    freeing data struct now zeros it out

--- src/data.c
+++ src/data.c
@@ -12,7 +12,9 @@ Data *create_data(int iMoney, int iItems, int iDebt) {
 
 FError free_data(Data *data) {
     if (!data) {return FFAILURE;}
-
+    data->money=0;
+    data->items=0;
+    data->debt=0;
     free(data);
     return FSUCCESS;
 }
