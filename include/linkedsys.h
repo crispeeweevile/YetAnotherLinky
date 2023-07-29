ae318a2fb45f9e0feb8281c2e39759f818c92469
crispeeweevile <crispeeweevile@gmail.com>
2023-07-29 01:29:45 -0500

    included some more headers added function protos

--- include/linkedsys.h
+++ include/linkedsys.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <stdbool.h>
 #include "node.h"
+#include "ferror.h"
 
 typedef struct SLList {
     Node *current;
@@ -7,4 +9,10 @@ typedef struct SLList {
     Node *tail;
 } LList;
 
+LList *create_llist(Node *node);
+FError free_llist(LList *list, bool bFreeData);
+FError insert_at_tail(LList *list, Node *node);
+Node *remove_tail(LList *list);
 
+FError goto_next(LList *list);
+FError has_next(LList *list);
