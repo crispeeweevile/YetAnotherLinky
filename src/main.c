4c9db060ef592b16b6bd9deedd98d89e0dbdee1b
crispeeweevile <crispeeweevile@gmail.com>
2023-07-29 02:14:22 -0500

    now testing goto_prev

--- src/main.c
+++ src/main.c
@@ -2,18 +2,20 @@
 #include "linkedsys.h"
 
 int main(int arc, char *arv[]) {
-    while (true){
+    {
         LList *tList = create_llist(create_node(create_data(1, 2, 3)));
         print_node(tList->current, true);
         insert_at_tail(tList, create_node(create_data(4, 5, 6)));
         goto_next(tList);
         print_node(tList->current, true);
+        goto_prev(tList);
+        print_node(tList->current, true);
         if (free_llist(tList, true) != FSUCCESS) {
             printf("cocks\n");
             return 1;
-        }
-        printf("current: %p; head: %p; tail: %p\n", tList->current, tList->head, tList->tail);
-        tList = NULL;
+        } else {tList = NULL;}
+        //printf("current: %p; head: %p; tail: %p\n", tList->current, tList->head, tList->tail);
+        //tList = NULL;
     }
 
     return 0;
