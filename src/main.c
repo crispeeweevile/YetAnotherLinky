a4c49521708a0e05f486eaf12da1e3d748926701
crispeeweevile <crispeeweevile@gmail.com>
2023-07-29 02:06:16 -0500

    better testing (functional version)

--- src/main.c
+++ src/main.c
@@ -2,9 +2,18 @@
 #include "linkedsys.h"
 
 int main(int arc, char *arv[]) {
-    {
+    while (true){
         LList *tList = create_llist(create_node(create_data(1, 2, 3)));
         print_node(tList->current, true);
+        insert_at_tail(tList, create_node(create_data(4, 5, 6)));
+        goto_next(tList);
+        print_node(tList->current, true);
+        if (free_llist(tList, true) != FSUCCESS) {
+            printf("cocks\n");
+            return 1;
+        }
+        printf("current: %p; head: %p; tail: %p\n", tList->current, tList->head, tList->tail);
+        tList = NULL;
     }
 
     return 0;
